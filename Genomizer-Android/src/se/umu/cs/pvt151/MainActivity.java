26c113dd8298ca32ccc6aa02d7219b76941cd46a
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-04-25 11:27:40 +0200

    fix

--- Genomizer-Android/src/se/umu/cs/pvt151/MainActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/MainActivity.java
@@ -17,13 +17,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-		
-		ArrayList<String> hello = new ArrayList<String>();
-		
-=======
 
->>>>>>> branch 'master' of https://github.com/genomizer/genomizer-android.git
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();

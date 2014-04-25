db605f228d0979c3bbd0b1b63a642e113dbe96bd
Cillapower <cecilia.lindmark@gmail.com>
2014-04-25 13:39:07 +0200

    HELLOOOO!!!!

--- Genomizer-Android/src/se/umu/cs/pvt151/MainActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		String hello;
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()

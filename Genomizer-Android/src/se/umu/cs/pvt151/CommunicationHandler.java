e295b1bb7aa657957a2550c252411c1353fc2496
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-04-29 11:11:52 +0200

    fix

--- Genomizer-Android/src/se/umu/cs/pvt151/CommunicationHandler.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/CommunicationHandler.java
@@ -4,14 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.Socket;
 import java.net.URL;
 
-import org.apache.http.impl.io.ChunkedInputStream;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import android.util.Log;
 
 public class CommunicationHandler {
 	

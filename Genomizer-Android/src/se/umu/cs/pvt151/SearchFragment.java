9fe678e99dafc8e7a41a9d60a4496ed4b5c971e5
DonRosa <anders75lundberg@hotmail.com>
2014-04-29 15:35:55 +0200

    Fixat search annotations

--- Genomizer-Android/src/se/umu/cs/pvt151/SearchFragment.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SearchFragment.java
@@ -3,15 +3,25 @@ package se.umu.cs.pvt151;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class SearchFragment extends Fragment {
 	
+	private EditText expIdText;
+	private EditText pubIdText;
+	private EditText speciesText;
+	private EditText sexText;
+	private EditText genRelText;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 	}
 	
 	@Override
@@ -19,7 +29,46 @@ public class SearchFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_search, container, false);
 		
+		expIdText = createExpIdText();
+		pubIdText = createPubIdText();
+		speciesText = createSpeciesText();
+		sexText = createSexText();
+		genRelText = createGenRelText();
+		
+		
+		
 		return v;
+	}
+	
+	private EditText createGenRelText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private EditText createSexText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private EditText createSpeciesText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private EditText createPubIdText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private EditText createExpIdText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.menu_search, menu);
 	}
 	
 	

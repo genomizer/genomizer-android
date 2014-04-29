244e5ce28ba513ef5923accbf7dfcf3f9bae17fa
erik <erik@client225-213.wireless.umu.se>
2014-04-29 15:08:02 +0200

    Added files

--- Genomizer-Android/src/se/umu/cs/pvt151/com/ComHandler.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/com/ComHandler.java
@@ -1,5 +1,58 @@
 package se.umu.cs.pvt151.com;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import se.umu.cs.pvt151.CommunicationHandler;
+import android.util.Log;
+
 public class ComHandler {
 
+	private static String serverURL = "http://genomizer.apiary-mock.com/";
+
+	
+	public static boolean login(String username, String password) {
+		
+		
+		try {
+			Log.d("DEBUG", "Creating communicator");
+			Communicator communicator = new Communicator(serverURL + "login");
+			
+			Log.d("DEBUG", "Communicator created");
+			
+			Log.d("DEBUG", "Creating msg");
+			JSONObject msg = MsgFactory.createLogin(username, password);
+			
+			Log.d("DEBUG", "Sending msg");
+			communicator.sendRequest(msg);
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			Log.d("DEBUG", e.getMessage());
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			Log.d("DEBUG", e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Log.d("DEBUG", e.getMessage());
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public static List<String[]> search(List<String[]> annotations) {
+		
+		
+		return null;
+	}
+	
+	
 }
--- Genomizer-Android/src/se/umu/cs/pvt151/com/Communicator.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/com/Communicator.java
@@ -1,5 +1,71 @@
 package se.umu.cs.pvt151.com;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONObject;
+
+import android.util.Log;
+
 public class Communicator {
 
+	
+	private HttpURLConnection connection;
+	private DataOutputStream out;
+	
+	public Communicator(String urlString) throws IOException {
+		setupConnection(urlString);
+	}
+
+	public void setupConnection(String urlString) throws IOException {
+		URL url = new URL(urlString);
+		Log.d("DEBUG", urlString);
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setDoOutput(true);
+		connection.setDoInput (true);
+		connection.setRequestMethod("POST");
+		connection.setUseCaches(false);
+		connection.setRequestProperty("Content-Type", "application/json");
+		connection.setRequestProperty("Accept", "application/json");
+		connection.setChunkedStreamingMode(100);
+		
+		connection.setConnectTimeout(3000);
+		connection.setReadTimeout(3000);
+		
+		out = new DataOutputStream(connection.getOutputStream());
+
+	}
+	
+	public int sendRequest(JSONObject jsonPackage) throws IOException {
+		
+		byte[] pack = jsonPackage.toString().getBytes("UTF-8");
+		
+		Log.d("DEBUG", "SENDING: " + pack.toString());
+		out.write(pack);
+		out.flush();
+		
+		int responseCode = connection.getResponseCode();
+		Log.d("DEBUG", "Response: " + responseCode);
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(connection.getInputStream()));
+		
+		StringBuffer response = new StringBuffer();
+		String inputLine;
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		System.out.println(response.toString());
+		Log.d("DEBUG", "Communicator response: "+ response.toString());
+			
+		return responseCode;
+	}
 }

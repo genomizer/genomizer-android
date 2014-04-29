a101c74fa93bdfa35dd6256d25611a7dd5a55453
Cillapower <cecilia.lindmark@gmail.com>
2014-04-29 11:27:20 +0200

    Created FileListActivity, FileListFragment, started designing little

---
+++ Genomizer-Android/src/se/umu/cs/pvt151/FileListActivity.java
@@ -0,0 +1,31 @@
+package se.umu.cs.pvt151;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class FileListActivity extends SingleFragmentActivity {
+
+	FileListFragment fragment;
+	@Override
+	protected Fragment createFragment() {
+		fragment = new FileListFragment();
+		return fragment;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.file_list, menu);
+		return true;
+	}
+
+}

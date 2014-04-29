fc5539813a89cf4364230f7d14e57cde8bd36bc3
erik <erik@client225-213.wireless.umu.se>
2014-04-29 10:04:52 +0200

    MsgFactory

--- Genomizer-Android/src/se/umu/cs/pvt151/com/MsgFactory.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/com/MsgFactory.java
@@ -1,5 +1,35 @@
 package se.umu.cs.pvt151.com;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MsgFactory {
 
+	private static String serverURL = "http://genomizer.apiary-mock.com";
+	private static MsgFactory factoryReference = null;
+	
+	private MsgFactory() {
+				
+	}
+	
+	public static MsgFactory getFactoryInstance() {
+		if(factoryReference == null) {
+			return new MsgFactory();
+		} 
+		return factoryReference;
+	}
+	
+	public static void changeServerURL(String url) {
+		serverURL = url;
+	}
+	
+	public static String createLogin(String username, String password) throws JSONException {
+		JSONObject obj = new JSONObject();
+		obj.put("username", username);
+		obj.put("password", password);
+		
+		return null;
+	}
+	
+	
 }

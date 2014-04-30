3b53b3f43754778c363a143c28db2c3d6809add7
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-04-30 10:50:38 +0200

    småändringar, lagt till exception

--- Genomizer-Android/src/se/umu/cs/pvt151/com/MsgDeconstructor.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/com/MsgDeconstructor.java
@@ -1,5 +1,12 @@
 package se.umu.cs.pvt151.com;
 
-public class MsgDeconstructor {
+import org.json.JSONObject;
 
+public class MsgDeconstructor {
+	
+	
+	public static JSONObject deconstructLogin() {
+		return null;
+		
+	}
 }

86325570d1cf04d10dfe1090f9cb8cc0c020b831
DonRosa <anders75lundberg@hotmail.com>
2014-05-01 13:11:12 +0200

    SearchButton added

--- Genomizer-Android/src/se/umu/cs/pvt151/SearchListFragment.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SearchListFragment.java
@@ -2,11 +2,17 @@ package se.umu.cs.pvt151;
 
 import java.util.ArrayList;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
@@ -16,9 +22,6 @@ import android.widget.Toast;
 
 public class SearchListFragment extends ListFragment {
 	
-	private String[] values = new String[] { "Experiment Id", "Pubmed Id", "Type of data",
-	        "Species", "Genome release", "Cell-line", "Development stage", "Sex",
-	        "Tissue", "Antigen name", "Antigen symbol", "Antibody" };
 
 	private ArrayList<String> annotationList;
 
@@ -26,24 +29,52 @@ public class SearchListFragment extends ListFragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		populateAnnotation();
+		
+		
+	}
+
+	/**
+	 * Populates the annotation fields while there is no direct uplonk to the
+	 * server, should be removed and replaced after the server can respond with
+	 * actual annotations.
+	 */
+	private void populateAnnotation() {
+		String[] values = new String[] { "Experiment Id", "Pubmed Id",
+				"Type of data", "Species", "Genome release", "Cell-line",
+				"Development stage", "Sex", "Tissue", "Antigen name",
+				"Antigen symbol", "Antibody" };
 		annotationList = new ArrayList<String>();
 		for (String s : values) {
 			annotationList.add(s);
 		}
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		ListView listView = getListView();
+		View footer = getActivity().getLayoutInflater().inflate(
+				R.layout.searchlist_footer, null);
+		Button searchButton = (Button) footer.findViewById(R.id.btn_search_footer);
+		searchButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//TODO remove testprint
+				Log.d("smurf", "Search....");
+				
+			}
+		});
+		
+		
+		listView.addFooterView(footer);
+		Log.d("smurf", "added footer");
 		
 		SearchListAdapter adapter = new SearchListAdapter(annotationList);
 		adapter.setNotifyOnChange(true);
 		setListAdapter(adapter);
-		
-		if (annotationList.isEmpty()) {
-			Toast.makeText(getActivity(), "No stores to display, add store by using the menu",
-					Toast.LENGTH_LONG).show();
-		}
-	}
 
-	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
 	}
 	
 	@Override

bfb7b8990c7f923c03e6b90b528d6274f0063f98
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-05-07 09:31:28 +0200

    fixade mottagandet av paket. skapade klassen GenomizerHttpPackage

--- Genomizer-Android/src/se/umu/cs/pvt151/com/ComHandler.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/com/ComHandler.java
@@ -41,16 +41,17 @@ public class ComHandler {
 
 			JSONObject msg = MsgFactory.createLogin(username, password);
 			communicator.setupConnection("POST");
-			
-			String jsonString = communicator.sendRequest(msg);
-			
-			if (jsonString != null) {
+			GenomizerHttpPackage loginResponse = communicator.sendRequest(msg);
+			if (loginResponse.getCode() == 200) {
+				String jsonString = loginResponse.getBody();
 				JSONObject jsonPackage = new JSONObject(jsonString);
 				communicator.setToken(jsonPackage.get("token").toString());
 				return true;
 			} else {
 				return false;
 			}
+
+
 
 		} catch (JSONException e) {
 			Log.d("DEBUG", e.getMessage());
@@ -72,29 +73,29 @@ public class ComHandler {
 	 */
 	public static JSONArray search(HashMap<String, String> annotations) throws IOException {
 		try {
-			//TODO FIX THIS LINE - NEEDS ANNOTATIONS
 			Communicator communicator = new Communicator(serverURL + "search/annotations=?"+generatePubmedQuery(annotations));
 			communicator.setupConnection("GET");
-			
+
 			JSONObject msg = MsgFactory.createRegularPackage();
-			
-			String jsonString = communicator.sendRequest(msg);
-			
-			if (jsonString != null) {
+
+			GenomizerHttpPackage searchResponse = communicator.sendRequest(msg);
+
+			if (searchResponse.getCode() == 200) {
+				String jsonString = searchResponse.getBody();
 				JSONArray jsonPackage = new JSONArray(jsonString);
-				Log.d("DEBUG", jsonPackage.toString());
+				return jsonPackage;
+			} else if (searchResponse.getCode() == 204) { 
+				//If the search yields no result.
+				JSONArray jsonPackage = new JSONArray();
 				return jsonPackage;
 			} else {
-				Log.d("DEBUG", "ComHandler - NULL json paket response");
 				return null;
 			}
-			
 		} catch (JSONException e) {
-			Log.d("DEBUG", "ComHandler(search) JSONException: " + e.getMessage());
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Returns the Annotations of the server.
 	 * 
@@ -103,29 +104,30 @@ public class ComHandler {
 	 */
 	public static ArrayList<Annotation> getServerAnnotations() throws IOException {
 		try {
-		
+
 			Communicator communicator = new Communicator(serverURL + "annotation");
 			communicator.setupConnection("GET");
 			JSONObject msg = MsgFactory.createRegularPackage();
-			
-			String jsonString = communicator.sendRequest(msg);
-			
-			if (jsonString != null) {
+
+			GenomizerHttpPackage annotationResponse = communicator.sendRequest(msg);
+
+			if (annotationResponse.getCode() == 200) {
+				String jsonString = annotationResponse.getBody();
 				JSONArray jsonPackage = new JSONArray(jsonString);
-				
+
 				Log.d("DEBUG", jsonPackage.toString());
 				return MsgDeconstructor.annotationJSON(jsonPackage);
 			} else {
 				Log.d("DEBUG", "ComHandler - NULL json paket response");
 				return null;
 			}
-			
+
 		} catch (JSONException e) {
 			Log.d("DEBUG", "ComHandler(annotations) JSONException: " + e.getMessage());
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Returns a pubmed query string ready to be put in a URL. It is encoded for URLs so it cannot be used elsewhere.
 	 * 
@@ -135,7 +137,7 @@ public class ComHandler {
 	 */
 	public static String generatePubmedQuery(HashMap<String, String> annotations) throws UnsupportedEncodingException {
 		String pubmedQuery = "<";
-		
+
 		Set<String> ann = annotations.keySet();
 		int i = 0;
 		for (String searchWord : ann) {
@@ -148,21 +150,23 @@ public class ComHandler {
 		}
 		pubmedQuery += ">";
 		return URLEncoder.encode(pubmedQuery, "UTF-8");
-		
+
 	}
-	
-	
+
+
 	public static String rawToProfile(String fileID) throws IOException {
-		
+
 		try {
 			Communicator communicator = new Communicator(serverURL + "process/rawtoprofile/" + fileID);
 			communicator.setupConnection("PUT");
 			JSONObject msg = MsgFactory.createRegularPackage();
+
+			GenomizerHttpPackage annotationResponse = communicator.sendRequest(msg);
 			
-			String jsonString = communicator.sendRequest(msg);
-			
+			String jsonString = annotationResponse.getBody();
+
 			return jsonString;
-			
+
 		} catch (JSONException e) {
 			return null;
 		}

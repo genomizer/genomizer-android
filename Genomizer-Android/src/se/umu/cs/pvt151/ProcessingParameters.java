0ca6bddcf220dafdc763101d9d209b64e40d528b
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-05-07 10:29:31 +0200

    processing parameters

---
+++ Genomizer-Android/src/se/umu/cs/pvt151/ProcessingParameters.java
@@ -0,0 +1,33 @@
+package se.umu.cs.pvt151;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * 
+ * @author Rickard
+ *
+ */
+public class ProcessingParameters {
+	
+	private ArrayList<String> parameters;
+	
+	public ProcessingParameters() {
+		parameters = new ArrayList<String>();
+	}
+	
+	
+	public void addParameter(String parameter) {
+		parameters.add(parameter);
+	}
+	
+	
+	public String getParameter(int index) {
+		return parameters.get(index);
+	}
+	
+	
+	public int size() {
+		return parameters.size();
+	}
+}
--- Genomizer-Android/src/se/umu/cs/pvt151/com/MsgFactory.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/com/MsgFactory.java
@@ -4,7 +4,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import se.umu.cs.pvt151.GeneFile;
-import se.umu.cs.pvt151.ProcessParameters;
+import se.umu.cs.pvt151.ProcessingParameters;
 
 public class MsgFactory {
 
@@ -35,11 +35,16 @@ public class MsgFactory {
 	}
 	
 	
-	public static JSONObject createConversionRequest(ProcessParameters param, GeneFile file) throws JSONException {
+	public static JSONObject createConversionRequest(ProcessingParameters param, GeneFile file, 
+			String metadata, String author) throws JSONException {
+		
 		JSONObject obj = new JSONObject();
 		
 		obj.put("filename", file.getName());
-		obj.put("filename", file.getName());
+		obj.put("filepath", file.getURL());
+		
+		//TODO: FIX this method when we know how to create process packages
+		obj.put("expid", file.getURL());
 		
 		return obj;
 	}

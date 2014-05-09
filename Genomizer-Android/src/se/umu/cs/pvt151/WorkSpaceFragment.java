829266c8f499fb40e00312bddb44d782a21764fd
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-05-09 16:05:59 +0200

    fixade workspace lite

--- Genomizer-Android/src/se/umu/cs/pvt151/WorkSpaceFragment.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/WorkSpaceFragment.java
@@ -1,23 +1,93 @@
 package se.umu.cs.pvt151;
 
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class WorkSpaceFragment extends Fragment {
-	
-	
-	public void onCreate(Bundle onSavedInstanceState) {
-		
+
+	private ListView listRaw;
+	private ListView listProfile;
+	private ListView listRegion;
+	private ListView listResults;
+
+	private ArrayList<GeneFile> raw;
+	private ArrayList<GeneFile> profile;
+	private ArrayList<GeneFile> region;
+	private ArrayList<GeneFile> results;
+
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		raw = new ArrayList<GeneFile>();
+		profile = new ArrayList<GeneFile>();
+		region = new ArrayList<GeneFile>();
+		results = new ArrayList<GeneFile>();
 	}
-	
-	
+
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_work_space, parent, false);
-		
+
+		listRaw = (ListView) v.findViewById(R.id.raw);
+		listProfile = (ListView) v.findViewById(R.id.profile);
+		listRegion = (ListView) v.findViewById(R.id.region);
+		listResults = (ListView) v.findViewById(R.id.result);
+
+		listRaw.setAdapter(new FileListAdapter(raw, "raw"));
+		listProfile.setAdapter(new FileListAdapter(profile, "profile"));
+		listRegion.setAdapter(new FileListAdapter(region, "region"));
+		listResults.setAdapter(new FileListAdapter(results, "results"));
+
 		return v;
+	}
+
+
+	/**
+	 * Adapter used for listviews
+	 *
+	 */
+	private class FileListAdapter extends ArrayAdapter<GeneFile> {
+		//TODO: Use same adapter for all three listviews, or need to make 3 differents ones?
+		ArrayList<GeneFile> forShow = new ArrayList<GeneFile>();
+		boolean[] selectedItem;
+		String data;
+
+		public FileListAdapter(ArrayList<GeneFile> fileInfo, String dataType) {
+			super(getActivity(), 0, fileInfo);
+			forShow = fileInfo;
+			data = dataType;
+			selectedItem = new boolean[fileInfo.size()];
+			for(int i = 0; i<selectedItem.length; i++) {
+				selectedItem[i] = false;
+			}
+		}
+		
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			
+			Context cont = getActivity();
+			
+			LayoutInflater inflater = (LayoutInflater) cont
+			        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View v = inflater.inflate(R.layout.list_view_checkbox, parent, false);
+			
+			TextView textView = (TextView) v.findViewById(R.id.textView1);
+			CheckBox checkBox = (CheckBox) v.findViewById(R.id.textForBox);
+			
+			
+			
+			return parent;
+		}
 	}
 }

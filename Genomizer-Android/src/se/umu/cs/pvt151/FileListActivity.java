048590ecab0cec2b6dd53879b582d210b284c385
Cillapower <cecilia.lindmark@gmail.com>
2014-05-14 11:51:12 +0200

    Changed menu

--- Genomizer-Android/src/se/umu/cs/pvt151/FileListActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/FileListActivity.java
@@ -1,18 +1,64 @@
 package se.umu.cs.pvt151;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class FileListActivity extends SingleFragmentActivity {
 
 	FileListFragment fragment;
-
+	
 	@Override
 	protected Fragment createFragment() {
 		fragment = new FileListFragment();
 		return fragment;
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.file_list, menu);
+	
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent i;
+		
+		switch (item.getItemId()) {
+		case R.id.btnsearch_main_menu:
+			i = new Intent(this, SearchActivity.class);
+			startActivity(i);
+			return true;
+		
+		case R.id.btnworkspace_main_menu:
+			Intent intent = new Intent(this,
+					WorkSpaceActivity.class);
+			startActivity(intent);
+			return true;
+			
+		case R.id.action_settings:
+			i = new Intent(this, SettingsActivity.class);
+			startActivity(i);
+			return true;
+			
+		/*case R.id.btnconvert_main_menu:
+			i = new Intent(this, ConvertActivity.class);
+			startActivity(i);
+			return true;*/
+			
+		default:
+			break;
+		}
+		
+		return super.onOptionsItemSelected(item);
 	}
 
 

ca50452b4fd45fd12d2c86703c53c957736c97fc
erik <erik@client225-218.wireless.umu.se>
2014-05-14 09:50:22 +0200

    Comments

--- Genomizer-Android/src/se/umu/cs/pvt151/LogInActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/LogInActivity.java
@@ -7,7 +7,11 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.support.v4.app.Fragment;
 import android.view.View;
-
+/**
+ * Activity which creates a LogInFragment and handles some of the logic involved in the process
+ * of logging in.
+ *
+ */
 public class LogInActivity extends SingleFragmentActivity {
 	
 	private static final String INTERNET_ACCESS_DENIED = "Internet access unavailable.";
@@ -15,18 +19,29 @@ public class LogInActivity extends SingleFragmentActivity {
 	private static final String CONNECT = "Connect";
 	LogInFragment fragment;
 
+	/**
+	 * Called automatically when activity is created. Creates a
+	 * LogInFragment and sets the server url in the ComHandler.
+	 */
 	protected Fragment createFragment() {
 		fragment = new LogInFragment();
 		getSavedServerURL();
 		return fragment;
 	}
 
+	/**
+	 * Fetch serverURL string from phone storage. Update ComHandler server with url.
+	 */
 	private void getSavedServerURL() {
 		SharedPreferences settings = this.getSharedPreferences("Settings",Context.MODE_PRIVATE);
 		ComHandler.setServerURL(settings.getString("url", ComHandler.getServerURL()));
 		
 	}
 	
+	/**
+	 * Verify that the phone currently have an internet connection.
+	 * @return true if online false otherwise
+	 */
 	private boolean isOnline() {
 	    ConnectivityManager cm =
 	        (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -35,6 +50,11 @@ public class LogInActivity extends SingleFragmentActivity {
 	       cm.getActiveNetworkInfo().isConnected();
 	}
 	
+	/**
+	 * Method is called when pressing the "Sign in" button in the user
+	 * interface. Starts a background thread which starts the log in process.
+	 * @param v
+	 */
 	public void login(View v) {
 		if(isOnline()) {
 			ProgressDialog progress = new ProgressDialog(this);

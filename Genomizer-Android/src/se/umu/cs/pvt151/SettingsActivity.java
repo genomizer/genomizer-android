5865a0482a4a4eeefd2dbfd62cd1adfe2157fc88
erik <erik@client224-198.wireless.umu.se>
2014-05-15 11:16:16 +0200

    SettingsFragment fix

--- Genomizer-Android/src/se/umu/cs/pvt151/SettingsActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SettingsActivity.java
@@ -5,9 +5,16 @@ import android.support.v4.app.Fragment;
 
 public class SettingsActivity extends SingleFragmentActivity {
 
+	private SettingsFragmentAlternative settingsFragment = null;
 	@Override
 	protected Fragment createFragment() {		
+		settingsFragment = new SettingsFragmentAlternative();
+		//return settingsFragment;
 		return new SettingsFragment();
+	}
+	
+	public void editURLs() {
+		settingsFragment.onSaveButtonClick();
 	}
 
 
--- Genomizer-Android/src/se/umu/cs/pvt151/SettingsFragment.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SettingsFragment.java
@@ -1,5 +1,7 @@
 package se.umu.cs.pvt151;
 
+import java.util.ArrayList;
+
 import se.umu.cs.pvt151.com.ComHandler;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,14 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
 	private Spinner spinner;
+	private final String settingsURLanchor = "selectedURL";
+	private final String settingsAllURLsAnchor = "savedURLs";
+	private ArrayList<String> mSavedURLsList = new ArrayList<String>();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,40 +27,18 @@ public class SettingsFragment extends Fragment {
 		View view = inflater.inflate(R.layout.fragment_settings, container,
 				false);
 		createSpinner(view);
-		createAdapter();
-		
-
+		markCurrentlyUsedURL();		
+		view.findViewById(R.id.settingsEditButton).setVisibility(4);
 		return view;
 	}
-
 
 	private void createSpinner(View view) {
 		spinner = (Spinner) view.findViewById(R.id.spinner1);
 		((TextView) view.findViewById(R.id.settings_textview))
 				.setText("Select server url");
 		
-		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-
-			@Override
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int position, long id) {
-				String serverURL = (String) spinner.getSelectedItem();
-				ComHandler.setServerURL(serverURL);
-				spinner.setSelection(position);
-				saveSettings(serverURL);
-			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {			
-			}
-		});
-		
-		String[] servers = getResources().getStringArray(R.array.server_list);
-		for (int i = 0; i < servers.length; i++) {
-			if (servers[i].compareTo(ComHandler.getServerURL()) == 0) {
-				spinner.setSelection(i);
-			}
-		}
+		createAdapter();		
+		spinner.setOnItemSelectedListener(this);				
 	}
 
 	private void createAdapter() {
@@ -64,16 +46,45 @@ public class SettingsFragment extends Fragment {
 				getActivity(), R.array.server_list,
 				android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
-
 		spinner.setAdapter(adapter);
 	}
-	
-	private void saveSettings(String url) {
-		SharedPreferences settings = getActivity().getSharedPreferences("Settings", 0);
-		SharedPreferences.Editor prefEditor = settings.edit();
-		prefEditor.putString("url", url);
-		prefEditor.commit();
+
+	private void markCurrentlyUsedURL() {
+		String currentURL = getSavedURL();
 		
+		String[] servers = getResources().getStringArray(R.array.server_list);
+		for (int i = 0; i < servers.length; i++) {
+			if (servers[i].compareTo(currentURL) == 0) {
+				spinner.setSelection(i);
+				mSavedURLsList.add(servers[i]);
+			}
+		}
+	}
+	
+	private void saveCurrentlyUsedURL(String url) {
+		SharedPreferences settings = getActivity().getSharedPreferences("Settings", 0);		
+		SharedPreferences.Editor prefEditor = settings.edit();
+		prefEditor.putString(settingsURLanchor, url);
+		prefEditor.commit();			
+	}
+	
+	private String getSavedURL() {
+		SharedPreferences settings = getActivity().getSharedPreferences("Settings", 0);		
+		return settings.getString(settingsURLanchor, ComHandler.getServerURL());
+	}
+
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int position,
+			long arg3) {
+		String serverURL = (String) spinner.getSelectedItem();
+		ComHandler.setServerURL(serverURL);
+		spinner.setSelection(position);
+		saveCurrentlyUsedURL(serverURL);				
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {				
 	}
 
 }

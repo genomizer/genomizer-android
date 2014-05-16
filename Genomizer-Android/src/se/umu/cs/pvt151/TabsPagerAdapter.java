2c8790dcf43c803c6c710da355008373aa80efde
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-05-16 12:38:50 +0200

    omgjort selected files, med swipe och nya fragment

---
+++ Genomizer-Android/src/se/umu/cs/pvt151/TabsPagerAdapter.java
@@ -0,0 +1,34 @@
+package se.umu.cs.pvt151;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+	public TabsPagerAdapter(FragmentManager fm) {
+		super(fm);
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		switch (position) {
+		case 0:
+			return new RawFragment();
+		case 1:
+			return new ProfileFragment();
+		case 2:
+			return new RegionFragment();
+		case 3:
+			return new ResultFragment();
+		}
+
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		return 4;
+	}
+
+}

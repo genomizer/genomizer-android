fdfd73c1fd4102181e92b5efaf8519b2465a5713
DonRosa <anders75lundberg@hotmail.com>
2014-05-16 23:42:58 +0200

    ConverterFragment, order of selection almost done.

--- Genomizer-Android/src/se/umu/cs/pvt151/ConverterFragment.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/ConverterFragment.java
@@ -5,16 +5,15 @@ package se.umu.cs.pvt151;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import se.umu.cs.pvt151.com.ComHandler;
 import se.umu.cs.pvt151.model.GenomeRelease;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -35,61 +34,159 @@ import android.widget.ToggleButton;
  *
  */
 public class ConverterFragment extends Fragment{
-	
-	private TextView txtBowtie;
-	private TextView txtGeneVer;
-	private TextView txtSamToGff;
-	private TextView txtGffToSgr;
-	private TextView txtSmoothing;
-	private TextView txtStepSize;
-	private TextView txtRatioCalc;
-	private TextView txtRatio;
-	private TextView txtRatioSmooth;
-	private EditText editBowtie;
-	private EditText editSmoothing;
-	private EditText editStepSize;
-	private EditText editRatio;
-	private EditText editRatioSmooth;
-	private ToggleButton toggleSamToGff;
-	private ToggleButton toggleGffToSam;
-	private ToggleButton toggleRatioCalc;
+
+	private ArrayList<View> viewList;
+	private String[] headers = new String[] { "Bowtie", "Geneome version",
+			"SAM to GFF", "GFF to SGR", "Smoothing", "Stepsize",
+			"Ratio calculation", "Ratio", "Smoothing" };
+	private String[] hints = new String[] { "-a -m 1 --best -p 10 -v 2 -q -S",
+			"10 1 5 0 0", "y 10", "single 4 0", "150 1 7 0 0" };
 	private Spinner spinnerGeneVer;
 	private Button convertButton;
-	private String[] headers = new String[] {"Bowtie", "Geneome version", "SAM to GFF", "GFF to SGR", "Smoothing", "Stepsize", "Ratio calculation", "Ratio", "Smoothing"};
-	private String[] hints = new String[] {"-a -m 1 --best -p 10 -v 2 -q -S", "10 1 5 0 0", "y 10", "single 4 0", "150 1 7 0 0"};
-	private ArrayList<EditText> editTextList;
+	private HashMap<String, Integer> viewMap;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_convert_params, container, false);
-		
-		setupTextViews(v);
-		setupEditTexts(v);
-		setupToggleButtons(v);
-		setupButton(v);
+
 		spinnerGeneVer = (Spinner) v.findViewById(R.id.spinner_convert_geneversion);
+
+		viewList = new ArrayList<View>();
+		viewList = setupTextViews(v, viewList);
+		viewList = setupEditTexts(v, viewList);
+		viewList = setupToggleButtons(v, viewList);
+		convertButton = setupButton(v);
+		viewMap = mapWidgets();
+
 		new GetGeneReleaseTask().execute();
 
 		return v;
 	}
 
-	
+
+
+	/**
+	 * 
+	 * @param v
+	 * @param viewList2
+	 * @return
+	 */
+	private ArrayList<View> setupTextViews(View v, ArrayList<View> viewList2) {
+		TextView tw;
+
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_bowtie));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_geneversion));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_samtogff));	
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_gfftosgr));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_smoothing));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_stepsize));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_ratiocalc));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_ratio));
+		viewList2.add((TextView) v.findViewById(R.id.lbl_convert_ratiosmooth));
+
+		for (int i = 0; i < headers.length; i++) {
+			tw = (TextView) viewList2.get(i);
+			tw.setText(headers[i]);
+		}
+
+		return viewList2;
+	}
+
+
 
 	/**
 	 * @param v
+	 * @param viewList2 
+	 * @return 
 	 */
-	private void setupButton(View v) {
-		convertButton = (Button) v.findViewById(R.id.btn_convert_convertbutton);
-		convertButton.setText("Convert");
-		convertButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+	private ArrayList<View> setupEditTexts(View v, ArrayList<View> viewList2) {
+		EditText et;
+		int i = viewList2.size();
+		int j = viewList2.size();
+
+		viewList2.add((EditText) v.findViewById(R.id.edit_convert_bowtie));
+		viewList2.add((EditText) v.findViewById(R.id.edit_convert_smoothing));
+		viewList2.add((EditText) v.findViewById(R.id.edit_convert_stepsize));
+		viewList2.add((EditText) v.findViewById(R.id.edit_convert_ratio));
+		viewList2.add((EditText) v.findViewById(R.id.edit_convert_ratiosmooth));
+
+		for (String s : hints) {
+			et = (EditText) viewList2.get(i);
+			et.setHint(s);
+			et.addTextChangedListener(new TextWatch(i));
+			if (i > j) {
+				et.setEnabled(false);
 			}
-		});
+			i++;
+		}
+		return viewList2;
 	}
+
+
+
+	/**
+	 * @param v
+	 * @param viewList2 
+	 * @return 
+	 */
+	private ArrayList<View> setupToggleButtons(View v, ArrayList<View> viewList2) {
+		int i = viewList2.size();
+		ToggleButton tb;
+
+		viewList2.add((ToggleButton) v.findViewById(R.id.toggle_convert_samtogff));
+		viewList2.add((ToggleButton) v.findViewById(R.id.toggle_convert_gfftosgr));
+		viewList2.add((ToggleButton) v.findViewById(R.id.toggle_convert_ratiocalc));
+
+		for (int j = i; j < viewList2.size(); j++) {
+			tb = (ToggleButton) viewList2.get(j);
+			tb.setOnCheckedChangeListener(new CheckerChange(j));
+			tb.setEnabled(false);
+		}
+
+		return viewList2;
+	}
+
+
+
+	/**
+	 * @param v
+	 * @return 
+	 */
+	private Button setupButton(View v) {
+		Button button = (Button) v.findViewById(R.id.btn_convert_convertbutton);
+		button.setText("Convert");
+		button.setOnClickListener(new buttonListener());
+
+		return button;
+	}
+
+	private HashMap<String, Integer> mapWidgets() {
+		HashMap<String, Integer> temp = new HashMap<String, Integer>();
+
+		temp.put("Bowtie header", 0);
+		temp.put("GeneVersion header", 1);
+		temp.put("SamToGff header", 2);
+		temp.put("GffToSSgr", 3);
+		temp.put("Smoothing header", 4);
+		temp.put("StepSize header", 5);
+		temp.put("RatioCalc header", 6);
+		temp.put("Ratio header", 7);
+		temp.put("RatioSmoothing header", 8);
+		temp.put("Bowtie edit", 9);
+		temp.put("Smoothing edit", 10);
+		temp.put("StepSize edit", 11);
+		temp.put("Ratio edit", 12);
+		temp.put("RatioSmoothing edit", 13);
+		temp.put("SamToGff toggle", 14);
+		temp.put("GffToSgr toggle", 15);
+		temp.put("RatioCalc toggle", 16);
+		temp.put("GeneVer spinner", 17);
+
+		return temp;
+	}
+
+
 
 	/**
 	 * 
@@ -103,124 +200,16 @@ public class ConverterFragment extends Fragment{
 		for (GenomeRelease genomeRelease : geneRelList) {
 			geneList.add(genomeRelease.getGenomeVersion());
 		}
+
 		adapter = new ArrayAdapter<String>(getActivity()
 				,
 				android.R.layout.simple_spinner_item, geneList);
 		spinnerGeneVer.setAdapter(adapter);
+		spinnerGeneVer.setOnItemSelectedListener(new itemListener());
+		viewList.add(spinnerGeneVer);
 	}
 
-	/**
-	 * @param v
-	 */
-	private void setupToggleButtons(View v) {
-		toggleSamToGff = (ToggleButton) v.findViewById(R.id.toggle_convert_samtogff);
-		toggleSamToGff.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		toggleGffToSam = (ToggleButton) v.findViewById(R.id.toggle_convert_gfftosgr);
-		toggleGffToSam.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		toggleRatioCalc = (ToggleButton) v.findViewById(R.id.toggle_convert_ratiocalc);
-		toggleRatioCalc.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-	}
-
-	/**
-	 * @param v
-	 */
-	private void setupEditTexts(View v) {
-		editTextList = new ArrayList<EditText>();
-		
-		editBowtie = (EditText) v.findViewById(R.id.edit_convert_bowtie);
-		editBowtie.setHint(hints[0]);
-		editBowtie.addTextChangedListener(new TextWatch(0));
-		editTextList.add(editBowtie);
-		
-		editSmoothing = (EditText) v.findViewById(R.id.edit_convert_smoothing);
-		editSmoothing.setHint(hints[1]);
-		editBowtie.addTextChangedListener(new TextWatch(1));
-		editTextList.add(editSmoothing);
-		
-		editStepSize = (EditText) v.findViewById(R.id.edit_convert_stepsize);
-		editStepSize.setHint(hints[2]);
-		editBowtie.addTextChangedListener(new TextWatch(2));
-		editTextList.add(editStepSize);
-		
-		editRatio = (EditText) v.findViewById(R.id.edit_convert_ratio);
-		editRatio.setHint(hints[3]);
-		editBowtie.addTextChangedListener(new TextWatch(3));
-		editTextList.add(editRatio);
-		
-		editRatioSmooth = (EditText) v.findViewById(R.id.edit_convert_ratiosmooth);
-		editRatioSmooth.setHint(hints[4]);
-		editBowtie.addTextChangedListener(new TextWatch(4));
-		editTextList.add(editRatioSmooth);
-		
-		for (EditText e : editTextList) {
-			e.setEnabled(false);
-		}
-		editTextList.get(0).setEnabled(true);
-	}
-
-	/**
-	 * @param v
-	 */
-	private void setupTextViews(View v) {
-		txtBowtie = (TextView) v.findViewById(R.id.lbl_convert_bowtie);
-		txtBowtie.setText(headers[0]);
-		
-		txtGeneVer = (TextView) v.findViewById(R.id.lbl_convert_geneversion);
-		txtGeneVer.setText(headers[1]);
-		
-		txtSamToGff = (TextView) v.findViewById(R.id.lbl_convert_samtogff);
-		txtSamToGff.setText(headers[2]);
-		
-		txtGffToSgr = (TextView) v.findViewById(R.id.lbl_convert_gfftosgr);
-		txtGffToSgr.setText(headers[3]);
-		
-		txtSmoothing = (TextView) v.findViewById(R.id.lbl_convert_smoothing);
-		txtSmoothing.setText(headers[4]);
-		
-		txtStepSize = (TextView) v.findViewById(R.id.lbl_convert_stepsize);
-		txtStepSize.setText(headers[5]);
-		
-		txtRatioCalc = (TextView) v.findViewById(R.id.lbl_convert_ratiocalc);
-		txtRatioCalc.setText(headers[6]);
-		
-		txtRatio = (TextView) v.findViewById(R.id.lbl_convert_ratio);
-		txtRatio.setText(headers[7]);
-		
-		txtRatioSmooth = (TextView) v.findViewById(R.id.lbl_convert_ratiosmooth);
-		txtRatioSmooth.setText(headers[8]);
-	}
-	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onActivityCreated(savedInstanceState);
-	}
-	
 	private class TextWatch implements TextWatcher {
-		
 		private int id;
 
 		public TextWatch(int id) {
@@ -230,31 +219,124 @@ public class ConverterFragment extends Fragment{
 		public void beforeTextChanged(CharSequence s, int start, int count,
 				int after) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void onTextChanged(CharSequence s, int start, int before,
 				int count) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void afterTextChanged(Editable s) {
-			Log.d("smurf", "s: " + s);
-			if (editTextList.size() > id && s.length() == 0) {
-				Log.d("smurf", "edittext: " + editTextList.size());
-//				editTextList.get(id + 1).setEnabled(false);
-			} else if (editTextList.size() > id) {
-				Log.d("smurf", "edittext: " + editTextList.size());
-//				editTextList.get(id + 1).setEnabled(true);
+			boolean enable = s.length() > 0;
+
+			switch (id) {
+			case 9:
+				viewList.get(viewMap.get("GeneVer spinner")).setEnabled(enable);
+				break;
+			case 10:
+				viewList.get(viewMap.get("StepSize edit")).setEnabled(enable);
+				break;
+			case 11:
+				viewList.get(viewMap.get("RatioCalc toggle")).setEnabled(enable);
+				break;
+			case 12:
+				
+				break;
+			case 13:
+
+				break;
+
+			default:
+				break;
 			}
+
+
+
+
+
+			if (s.length() > 0 && id == 0) {
+
+			} 
 		}
-		
+
 	}
+
+	/**
+	 * 
+	 * @author Anders
+	 *
+	 */
+	private class CheckerChange implements OnCheckedChangeListener {
+		private int id;
+
+		public CheckerChange(int id) {
+			this.id = id;
+		}
+
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView,
+				boolean isChecked) {
+
+			switch (id) {
+			case 14:
+				viewList.get(viewMap.get("GffToSgr toggle")).setEnabled(isChecked);
+				break;
+			case 15:
+				viewList.get(viewMap.get("Smoothing edit")).setEnabled(isChecked);
+				break;
+			case 16:
+				viewList.get(viewMap.get("Ratio edit")).setEnabled(isChecked);
+				viewList.get(viewMap.get("RatioSmoothing edit")).setEnabled(isChecked);
+				break;
+
+			default:
+				break;
+			}
+			// TODO Auto-generated method stub
+
+		}
+	}
+
+	private class itemListener implements OnItemSelectedListener {
+
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view,
+				int position, long id) {
+			viewList.get(viewMap.get("SamToGff toggle")).setEnabled(id > 0);
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	/**
+	 * 
+	 * @author Anders
+	 *
+	 */
+	private class buttonListener implements OnClickListener {
 	
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
 	
+		}
+	
+	}
+
+	/**
+	 * 
+	 * @author Anders
+	 *
+	 */
 	private class GetGeneReleaseTask extends AsyncTask<Void, Void, ArrayList<GenomeRelease>> {
 
 		private ArrayList<GenomeRelease> genomeList;
@@ -267,16 +349,16 @@ public class ConverterFragment extends Fragment{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			return null;
 		}
-		
+
 		@Override
 		protected void onPostExecute(ArrayList<GenomeRelease> result) {
 			super.onPostExecute(result);
 			setupSpinner(genomeList);
 		}
-		
+
 	}
-	
+
 }

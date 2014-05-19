60dcfae7e134ef32fe2c21d01f5c79420c2c542e
erik <erik@192.168.0.195>
2014-05-19 15:59:40 +0200

    asdasd

--- Genomizer-Android/src/se/umu/cs/pvt151/SearchActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SearchActivity.java
@@ -16,10 +16,7 @@ import android.view.MenuItem;
  */
 public class SearchActivity extends SingleFragmentActivity {
 
-	private static final String NEGATIVE_RESPONSE = "No";
-	private static final String POSITIVE_RESPONSE = "Yes";
-	private static final String EXIT = "Exit";
-	private static final String EXIT_QUERY = "Are you sure you want to exit?";
+	
 	private SearchListFragment searchListFrag;
 	private Menu menu;
 
@@ -45,24 +42,10 @@ public class SearchActivity extends SingleFragmentActivity {
 		return super.onMenuOpened(featureId, menu);
 	}
 	
-	/**
-	 * Handles the backbutton for the fragment, will ask the user if they
-	 * want to quit. If so the application will exit.
-	 */
+
 	@Override
 	public void onBackPressed() {
-		new AlertDialog.Builder(this)
-				.setIcon(android.R.drawable.ic_dialog_alert)
-				.setTitle(EXIT)
-				.setMessage(EXIT_QUERY)
-				.setPositiveButton(POSITIVE_RESPONSE,
-						new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								System.exit(0);
-							}
-						}).setNegativeButton(NEGATIVE_RESPONSE, null).show();
+		finish();
 	}
 
 }
--- Genomizer-Android/src/se/umu/cs/pvt151/SelectedFilesActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SelectedFilesActivity.java
@@ -1,11 +1,39 @@
 package se.umu.cs.pvt151;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
 
 public class SelectedFilesActivity extends SingleFragmentActivity {
+	
 	private SelectedFilesFragment selectedFilesFragment = null;
+	private static final String NEGATIVE_RESPONSE = "No";
+	private static final String POSITIVE_RESPONSE = "Yes";
+	private static final String EXIT = "Exit";
+	private static final String EXIT_QUERY = "Are you sure you want to exit?";
+	
 	public Fragment createFragment() {
 		selectedFilesFragment = new SelectedFilesFragment();
 		return selectedFilesFragment;
+	}
+	
+	/**
+	 * Handles the backbutton for the fragment, will ask the user if they
+	 * want to quit. If so the application will exit.
+	 */
+	@Override
+	public void onBackPressed() {
+		new AlertDialog.Builder(this)
+				.setIcon(android.R.drawable.ic_dialog_alert)
+				.setTitle(EXIT)
+				.setMessage(EXIT_QUERY)
+				.setPositiveButton(POSITIVE_RESPONSE,
+						new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								System.exit(0);
+							}
+						}).setNegativeButton(NEGATIVE_RESPONSE, null).show();
 	}
 }

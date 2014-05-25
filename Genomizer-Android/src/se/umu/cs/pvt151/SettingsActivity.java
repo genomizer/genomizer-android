f24cbdb6ce80484f281af16c10f1812d6c4ca80f
ErikAberg <erik.w.aberg@outlook.com>
2014-05-25 20:32:22 +0200

    Settings fragment rework. Settings fragment + settings activity have been commented and warnings removed.

--- Genomizer-Android/src/se/umu/cs/pvt151/SettingsActivity.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SettingsActivity.java
@@ -10,48 +10,85 @@ public class SettingsActivity extends SingleFragmentActivity  {
 	private SettingsFragment settingsFragment = null;
 	private Menu menu = null;
 
+	/**
+	 * Create a SettingsFragment which represents the view of this activity/fragment pair.
+	 */
 	@Override
 	protected Fragment createFragment() {				
 		settingsFragment = new SettingsFragment();		
 		return settingsFragment;
 	}
+	
+	
+	/**
+	 * Store the reference of the menu upon menu creation.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		this.menu = menu;
 		return super.onCreateOptionsMenu(menu);
 	}
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if(item.getTitle().toString().equals(getResources().getString(R.string.action_settings_edit)) || item.getTitle().toString().equals(getResources().getString(R.string.action_settings_add))) {
-			menu.clear();
-		} 
-		
-		settingsFragment.onMenuItemPress(item.getTitle().toString());
-		return super.onOptionsItemSelected(item);
-	}
-	
-	
-	
+	/**
+	 * Store the reference of the menu upon menu open, since menu may be placed within
+	 * a menu list or as icons on the action bar.
+	 */
 	@Override
 	public boolean onMenuOpened(int featureId, Menu menu) {
 		this.menu  = menu;
 		return super.onMenuOpened(featureId, menu);
 	}
 	
+	/**
+	 * Menu selection hook - triggers method within the settings fragment
+	 * to activate correct response.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {		
+		settingsFragment.onMenuItemPress(item.getTitle().toString());
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	
+	
+	/**
+	 * OnClick action listener for the multi-purpose button in the Settings view.
+	 * @param v
+	 */
 	public void onClickaddNewURL(View v) {
 		settingsFragment.onAddNewURLButtonClick();
 	}
+		
 	
+	/**
+	 * Provide the means of hiding menu items by setting them as invisible.
+	 */
+	public void clearMenuItems() {		
+		for(int i = 0; i < menu.size(); i++) {
+			menu.getItem(i).setVisible(false);		
+		}
+	}
+	
+	/**
+	 * Provide the means of restoring menu item visibility.
+	 */
 	public void restoreMenuItems() {
-		super.onCreateOptionsMenu(menu);
+		if(menu != null) {
+			for(int i = 0; i < menu.size(); i++) {
+				menu.getItem(i).setVisible(true);				
+			}
+		}		
 	}
 	
 	
+	/**
+	 * Make sure that server urls are saved even when user press the back button. 
+	 */
 	@Override
 	public void onBackPressed() {		
 		if(settingsFragment.isInEditMode()) {
-			settingsFragment.toggleEditMode();			
+			settingsFragment.setSelectionMode();			
 		} else {						
 			settingsFragment.saveMultipleURLs();
 			finish();
--- Genomizer-Android/src/se/umu/cs/pvt151/SettingsFragment.java
+++ Genomizer-Android/src/se/umu/cs/pvt151/SettingsFragment.java
@@ -24,65 +24,90 @@ import android.widget.Toast;
 
 public class SettingsFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
+	private static final String sButtonSaveSettings = "Save settings";
+	private static final String sButtonAddUrl = "Add URL";
+	private static final String sButtonSaveEdit = "Save changes";
+	private static final String sTextViewAddServer = "Enter a new server URL";
+	private static final String sTextViewSelectServer = "Select a server url";
+	private static final String sTextViewEditURL = "Edit server url";
 	private Spinner spinner;
 	private ArrayList<String> mSavedURLsList = new ArrayList<String>();
-	private Button addURLButton = null;
+	private Button multiUseButton = null;
 	private EditText urlEdit = null;
 	private final static int visibilityHide = 4;
 	private final static int visibilityShow = 0;
 	private View view = null;
-	private TextView textView = null;
-	private boolean inEditMode = false; 
+	private TextView textView = null;	
+	private InputMethodManager inputMethod = null;
 	
+	/**
+	 * Inflate the view and initiate all view items within it's scope.
+	 */
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_settings, container, false);
 		fetchSavedURLs();
 		this.view = view;
+
 		fetchViewItems();
-		createSpinner();						
-		markCurrentlyUsedURL();			
+		createSpinnerAdapter();						
+		markCurrentlyUsedURL();	
+		setSelectionMode();
 		return view;
 	}
 
-	private void createSpinner() {		
+	/**
+	 * Locate and store reference to view items associated with this fragment by id. 
+	 */
+	private void fetchViewItems() {
+		multiUseButton = (Button) view.findViewById(R.id.settingsAddButton);
+		urlEdit = (EditText) view.findViewById(R.id.settings_input_field);
 		spinner = (Spinner) view.findViewById(R.id.spinner1);
 		textView = ((TextView) view.findViewById(R.id.settings_textview));
-		textView.setText("Select a server url");
-		createAdapter();		
-		spinner.setOnItemSelectedListener(this);				
+		inputMethod = (InputMethodManager) getActivity().getSystemService(
+			      Context.INPUT_METHOD_SERVICE);
 	}
-	
-	private void createAdapter() {
+		
+	/**
+	 * Create an adapter for the Spinner(drop-down) element. Fill the Spinner with all URLs
+	 * stored within the 'mSavedURLsList'-list. 
+	 */
+	private void createSpinnerAdapter() {
 		ArrayAdapter<?> adapter = null;		
 		adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, mSavedURLsList);
 		adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
 		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(this);
 	}
-	
-	private void fetchViewItems() {
-		addURLButton = (Button) view.findViewById(R.id.settingsAddButton);
-		urlEdit = (EditText) view.findViewById(R.id.settings_input_field);
-		urlEdit.setText("http://");		
-	}
-	
+		
+	/**
+	 * Create a Toast message and place it in the middle of the screen.
+	 * @param text The message text that will be used in the Toast.
+	 */
 	private void makeToast(String text) {
 		Toast toast = Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT);
 		toast.setGravity( Gravity.CENTER_VERTICAL, 0, 0);
 		toast.show();
 	}
 	
+	/**
+	 * This method is called from the SettingsActivity upon selection of a menu item.
+	 * The menu items are used to switch between different contexts within the fragment and provide
+	 * the means of:
+	 * adding a new server URL
+	 * edit an existing server URL
+	 * remove an existing server URL
+	 * @param menuString The text representation of the selected menu-item.
+	 */
 	public void onMenuItemPress(String menuString) {
 		
 		if(menuString.equals("Add new URL")) {
-			toggleEditMode();
-		} else if(menuString.equals("Edit selected URL")) {
-			inEditMode = true;
-			urlEdit.setText(spinner.getSelectedItem().toString());
-			toggleEditMode();						
+			setAddMode();
+		} else if(menuString.equals("Edit selected URL")) {						
+			setEditMode();										
 		} else if(menuString.equals("Remove selected URL")) {
 			if(mSavedURLsList.size() == 1) {
-				makeToast("Please add a new server URL before you remove a server.");
+				makeToast("You need atleast 1 server to use this application. Please add a new server url before deleting.");
 				return;
 			} else {
 				new AlertDialog.Builder(getActivity()).setIcon(android.R.drawable.ic_dialog_alert)
@@ -97,7 +122,7 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
 							for(int i = 0; i < mSavedURLsList.size(); i++) {
 								if(mSavedURLsList.get(i).equals(urlToRemove)) {					
 									mSavedURLsList.remove(i);
-									createAdapter();
+									createSpinnerAdapter();
 									spinner.setSelection(0);
 									break;
 								}
@@ -111,60 +136,130 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
 		
 	}
 	
-	public void toggleEditMode() {
-		InputMethodManager inputMethod = (InputMethodManager) getActivity().getSystemService(
-			      Context.INPUT_METHOD_SERVICE);
-		if(!isInEditMode()) {
-			addURLButton.setText("Add URL");
-			urlEdit.setVisibility(visibilityShow);
-			textView.setText("Enter a new server URL.");
-			spinner.setVisibility(visibilityHide);
-			
-			urlEdit.setSelection(urlEdit.getText().length());			
-			inputMethod.toggleSoftInput(0, InputMethodManager.SHOW_IMPLICIT);
-		} else {
-			addURLButton.setText("Save Settings");
-			urlEdit.setVisibility(visibilityHide);
-			spinner.setVisibility(visibilityShow);		
-			textView.setText("Select server url");
-			inputMethod.hideSoftInputFromWindow(urlEdit.getWindowToken(), 0);
-			urlEdit.setText("http://");
-			((SettingsActivity) getActivity()).restoreMenuItems();
-		}
+	/**
+	 * Switches the view context of the fragment to 'edit mode'.
+	 * The edit mode enables the user to edit a selected server URL.
+	 * The software keyboard is triggered to SHOW.
+	 * The menu items are triggered to HIDE.
+	 */
+	private void setEditMode() {
+		multiUseButton.setText(sButtonSaveEdit);
+		urlEdit.setVisibility(visibilityShow);
+		textView.setText(sTextViewEditURL);
+		spinner.setVisibility(visibilityHide);		
+		urlEdit.setText(spinner.getSelectedItem().toString());
+		urlEdit.setSelection(urlEdit.getText().length());
+		((SettingsActivity) getActivity()).clearMenuItems();
+		inputMethod.toggleSoftInput(0, InputMethodManager.SHOW_IMPLICIT);
 	}
 	
+	/**
+	 * Switches the view context of the fragment to 'add mode'.
+	 * The add mode enables the user to add a new server URL.
+	 * The software keyboard is triggered to SHOW.
+	 * The menu items are triggered to HIDE.
+	 */
+	private void setAddMode() {
+		multiUseButton.setText(sButtonAddUrl);
+		urlEdit.setVisibility(visibilityShow);
+		textView.setText(sTextViewAddServer);
+		spinner.setVisibility(visibilityHide);	
+		urlEdit.setText("http://");
+		urlEdit.setSelection(urlEdit.getText().length());			
+		((SettingsActivity) getActivity()).clearMenuItems();
+		inputMethod.toggleSoftInput(0, InputMethodManager.SHOW_IMPLICIT);
+	}
+	
+	/**
+	 * Switches the view context of the fragment to 'selection mode'.
+	 * The selection mode enables the user to choose a server URL from a Spinner(drop-down) menu.
+	 * The software keyboard is triggered to HIDE.
+	 * The menu items are triggered to SHOW.
+	 */
+	public void setSelectionMode() {
+		multiUseButton.setText(sButtonSaveSettings);
+		urlEdit.setVisibility(visibilityHide);
+		spinner.setVisibility(visibilityShow);		
+		textView.setText(sTextViewSelectServer);
+		inputMethod.hideSoftInputFromWindow(urlEdit.getWindowToken(), 0);		
+		((SettingsActivity) getActivity()).restoreMenuItems();
+	}
+	
+	
+	/**
+	 * Is the user currently in 'edit-/add-mode' ?
+	 * @return true if the user is in either edit or add mode, otherwise false.
+	 */
 	public boolean isInEditMode() {
 		return spinner.getVisibility() == visibilityHide;
 	}
 	
+	/**
+	 * The 'multiUseButton' action listener. Upon click the view context of the fragment may change.
+	 */
 	public void onAddNewURLButtonClick() {
-		if(addURLButton.getText().equals("Add URL")) {
-			String newURL = urlEdit.getText().toString();
+	
+		if(multiUseButton.getText().equals(sButtonAddUrl)) {			
+			String newURL = urlEdit.getText().toString();				
 			if(!newURL.endsWith("/") ) {			
 				newURL += "/";
 			}
-			if(urlExists(newURL) ) {
-				makeToast(newURL + " already exists!");
-				toggleEditMode();
-			} else {
-				if(inEditMode) {
-					String strToEdit = spinner.getSelectedItem().toString();
-					mSavedURLsList.remove(strToEdit);
-				}
-				mSavedURLsList.add(newURL);
-				makeToast(newURL + " has been added!");
-				toggleEditMode();
-				saveCurrentlySelectedURL(newURL);
+			
+			addNewURL(newURL);						
+			setSelectionMode();
+			
+		} else if(multiUseButton.getText().equals(sButtonSaveEdit)) {
+			String newURL = urlEdit.getText().toString();
+			String strToEdit = spinner.getSelectedItem().toString();
+			
+			if(newURL.equals(strToEdit)) {
+				setSelectionMode();
+				return;
+			}
+			if(addNewURL(newURL)) {
+				mSavedURLsList.remove(strToEdit);
+				createSpinnerAdapter();
 				markCurrentlyUsedURL();
-			}	
-		} else {
+				setSelectionMode();
+				
+			}
+																					
+		} else if(multiUseButton.getText().equals(sButtonSaveSettings)){
 			getActivity().onBackPressed();
 		}
 		
 		
 	}
+
+	/**
+	 * Add a new URL to the 'mSavedURLsList' if it does not already exists.
+	 * If the URL already exists within the Spinner a Toast is triggered with an indicating this.
+	 * Otherwise, show a Toast message with indicating successful adding of URL and mark the newly added 
+	 * URL within the spinner. 
+	 * 
+	 * @param newURL
+	 * @return
+	 */
+	private boolean addNewURL(String newURL) {
+		if(urlExists(newURL)) {
+			makeToast(newURL + " already exists!");
+			return false;
+		} else {
+			mSavedURLsList.add(newURL);
+			makeToast(newURL + " has been added!");
+			setSelectionMode();
+			saveCurrentlySelectedURL(newURL);
+			markCurrentlyUsedURL();
+			return true;
+		}
+		
+	}
 	
 
+	/**
+	 * Fetch the currently selected URL from the settings file and mark this URL within
+	 * the Spinner (drop-down) menu.
+	 */
 	private void markCurrentlyUsedURL() {
 		String currentURL = getCurrentlySelectedURL();
 
@@ -175,6 +270,11 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
 		}
 	}
 	
+	/**
+	 * Check if an URL already exists with the 'mSavedURLsList' container.
+	 * @param url The server URL which the user wish to add.
+	 * @return true if the URL already exits, false otherwise.
+	 */
 	private boolean urlExists(String url) {		
 		for(String oldURL : mSavedURLsList) {
 			if(oldURL.equals(url)) {
@@ -184,27 +284,37 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
 		return false;
 	}
 	
+	/**
+	 * Write the currently selected URL to a settings file to enable persistence between application restarts.
+	 * @param url The server URL to store within the settings file.
+	 */
 	private void saveCurrentlySelectedURL(String url) {
-
 		SharedPreferences settings = getActivity().getSharedPreferences(getResources().getString(R.string.settings_fileAnchor) ,Context.MODE_PRIVATE);	
 		SharedPreferences.Editor prefEditor = settings.edit();		
 		prefEditor.putString(getResources().getString(R.string.settings_serverSelectedURLAnchor), url);
 		prefEditor.commit();			
 		
-		if(!urlExists(url)) {
-			mSavedURLsList.add(url);
-		}
 	}
 	
+	/**
+	 * Fetch the server URL that was previously stored within the settings file.
+	 * @return A string representing the URL of the selected server.
+	 */
 	private String getCurrentlySelectedURL() {
 		SharedPreferences settings = getActivity().getSharedPreferences(getResources().getString(R.string.settings_fileAnchor) ,Context.MODE_PRIVATE);	
 		return settings.getString(getResources().getString(R.string.settings_serverSelectedURLAnchor), ComHandler.getServerURL());
 	}
 	
+	/**
+	 * Locate all server URLs that have been saved within the settings file and store them in the 'mSavedURLsList'.
+	 * Also fetch the saved 'currently selected server URL' from the settings file and add it to the 'mSavedURLsList' if it is not
+	 * in the list.
+	 */
 	private void fetchSavedURLs() {		
 		SharedPreferences settings = getActivity().getSharedPreferences(getResources().getString(R.string.settings_fileAnchor) ,Context.MODE_PRIVATE);
 		String savedURLsWithDelimiters = settings.getString(getResources().getString(R.string.settings_serverALLURLAnchor), "#");
 		String[] savedURLs = savedURLsWithDelimiters.split("#");
+		
 		for(String url : savedURLs) {
 			mSavedURLsList.add(url);
 		}
@@ -221,6 +331,10 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
 		
 	}
 
+	/**
+	 * Save all server URLs that the user have added to a settings file. The server URLs are stored as a single string with 
+	 * the character '#' as separator.
+	 */
 	public void saveMultipleURLs() {
 		SharedPreferences settings = getActivity().getSharedPreferences(getResources().getString(R.string.settings_fileAnchor), Context.MODE_PRIVATE);	
 		SharedPreferences.Editor prefEditor = settings.edit();
@@ -229,12 +343,17 @@ public class SettingsFragment extends Fragment implements AdapterView.OnItemSele
 		for(String url : mSavedURLsList) {
 			sb.append(url);
 			sb.append('#');
-		}
+		}		
 		
 		prefEditor.putString(getResources().getString(R.string.settings_serverALLURLAnchor), sb.toString());
 		prefEditor.commit();				
 	}
 
+	/**
+	 * OnItemSelected method overriding the spinner-adapter action listener.
+	 * Upon server selection with the spinner, the server URL used in the ComHandler will be updated
+	 * and the URL will be stored to the settings file for reuse.
+	 */
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long arg3) {
 		String serverURL = (String) spinner.getSelectedItem();

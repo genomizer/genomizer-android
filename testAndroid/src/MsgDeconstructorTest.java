41c8983fa608a6de19ea883d2aa66c369006e5b2
Rickard Hjulström <r_hjulstrom23@hotmail.com>
2014-05-27 09:46:59 +0200

    test

--- testAndroid/src/MsgDeconstructorTest.java
+++ testAndroid/src/MsgDeconstructorTest.java
@@ -1,7 +1,19 @@
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import se.umu.cs.pvt151.com.ComHandler;
+import se.umu.cs.pvt151.com.Communicator;
+import se.umu.cs.pvt151.com.GenomizerHttpPackage;
+import se.umu.cs.pvt151.com.MsgDeconstructor;
+import se.umu.cs.pvt151.com.MsgFactory;
+import se.umu.cs.pvt151.model.Experiment;
 import junit.framework.TestCase;
 
 
@@ -24,23 +36,34 @@ public class MsgDeconstructorTest extends TestCase {
 	}
 	
 	
-//	public void testCanDeconstructSearchResults() {
-//		HashMap<String, String> searchValues = new HashMap<String, String>();
-//		searchValues.put("Species", "Human");
-//		try {
-//			ComHandler.login("John", "password");
-//			JSONArray experiments = ComHandler.search(searchValues);
-//			
-//			ArrayList<Experiment> experimentArray = MsgDeconstructor.searchJSON(experiments);
-//			
-//			assertNotNull(experimentArray);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//			fail("IOException!");
-//		} catch (JSONException e) {
-//			Log.d("TestLog", e.getMessage());
-//			fail("JSONException!");
-//			e.printStackTrace();
-//		}
-//	}
+	public void testDeconstructSearchResults() {
+		
+		
+		try {
+			Communicator.initCommunicator("http://genomizer.apiary-mock.com/");
+			
+			HashMap<String, String> annotations = new HashMap<String, String>();
+			annotations.put("Species", "Human");
+			
+			JSONObject msg = MsgFactory.createRegularPackage();
+			GenomizerHttpPackage searchResponse;
+			
+			searchResponse = Communicator.sendHTTPRequest
+					(msg, "GET", "search/?annotations=" + ComHandler.generatePubmedQuery(annotations));
+			
+			if (searchResponse.getCode() == 200) {
+				JSONArray jsonPackage = new JSONArray(searchResponse.getBody());
+				ArrayList<Experiment> experiments = MsgDeconstructor.deconSearch(jsonPackage);
+			}
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			fail("Failed because of unsupported encoding");
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Failed because of IOException");
+		} catch (JSONException e) {
+			fail("Failed because of JSONException");
+			e.printStackTrace();
+		}
+	}
 }
